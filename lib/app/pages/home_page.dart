8d26fb7adcb8ed1d4b65854159747f0024bec23f
Hugo Brandão <hugo.h.b@hotmail.com>
2020-08-24 11:27:08 -0300

    Included logo at top. Removed second page.

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/app/pages/overview_page.dart';
-import 'package:hello_world/app/pages/individual_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,26 +9,15 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Rachaê meu ermão"),
-          centerTitle: true,
-          bottom: TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.group)),
-              Tab(icon: Icon(Icons.person)),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'assets/images/Logo.png',
+          fit: BoxFit.scaleDown,
         ),
-        body: TabBarView(
-          children: [
-            OverviewPage(),
-            IndividualPage(),
-          ],
-        ),
+        centerTitle: true,
       ),
+      body: OverviewPage(),
     );
   }
 }
